927c395697d527c5c6815b929b653e36661170fa
dedsec <abdelaalatif253@gmail.com>
2023-08-01 15:08:07 +0200

    Update kratos.sh

--- kratos.sh
+++ kratos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Logo goes here...
 # 
+
+██╗░░██╗██████╗░░█████╗░████████╗░█████╗░
+██║░██╔╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗
+█████═╝░██████╔╝███████║░░░██║░░░██║░░██║
+██╔═██╗░██╔══██╗██╔══██║░░░██║░░░██║░░██║
+██║░╚██╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░
+
 #
 ### Script that customizes and installs necessary tools for debian linux to start and hacking and bug bounty hunting straight away.
 ### Coded by D4rkw1ng (Abdelaal Atif) v1.0
